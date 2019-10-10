e6c04e4ebb710202cb58d2275ca54a13eca0a06e
andres <luis.andradec14@gmail.com>
2019-10-09 23:36:28 -0500

    Primera versión del calculador
    esta es una versión que solo incluye las opciones de suma y resta.

---
+++ basicCalc.py
@@ -0,0 +1,29 @@
+# Programa que realiza operaciones matematicas básicas.
+# Esta función suma dos numeros
+def add(x, y):
+   return x + y
+# Esta funcion resta dos numeros
+def subtract(x, y):
+   return x - y
+
+print("Seleccion operacion")
+print("1.Suma")
+print("2.Resta")
+print("3.Multiplicación")
+print("4.División")
+# Tomar entrada
+choice = input("Ingrese selección(1/2/3/4):")
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   #print(num1,"*",num2,"=", multiply(num1,num2))
+   print("Proximamente")
+elif choice == '4':
+   #print(num1,"/",num2,"=", divide(num1,num2))
+   print("Proximamente")
+else:
+   print("Entrada no reconocida")
